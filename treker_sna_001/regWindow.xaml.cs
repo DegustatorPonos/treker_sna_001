b8ada8ad54b7ad19699d07c9a494c7f7197861d9
Ghost7227 <ivanvadimovichkovylin@gmail.com>
2025-04-05 10:20:31 +0500

    версия 2
    добавлено окно входа, удалена база данных

---
+++ treker_sna_001/regWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace treker_sna_001
+{
+    /// <summary>
+    /// Логика взаимодействия для regWindow.xaml
+    /// </summary>
+    public partial class regWindow : Window
+    {
+        public regWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtUser.Text == "test1user" && txtPass.Password == "test1pass")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
